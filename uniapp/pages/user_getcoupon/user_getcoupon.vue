9a250f4418e34990a421822f76ef3f6257d00830
wenjiexin <13609053257@163.com>
2021-05-24 10:20:35 +0800

    修改注释信息

--- uniapp/pages/user_getcoupon/user_getcoupon.vue
+++ uniapp/pages/user_getcoupon/user_getcoupon.vue
@@ -16,7 +16,7 @@
 // | 开源版本可自由商用，可去除界面版权logo
 // | 商业版本务必购买商业授权，以免引起法律纠纷
 // | 禁止对系统程序代码以任何目的，任何形式的再发布
-// | Gitee下载：https://gitee.com/likemarket/likeshopv2
+// | Gitee下载：https://gitee.com/likeshop_gitee/likeshop
 // | 访问官网：https://www.likemarket.net
 // | 访问社区：https://home.likemarket.net
 // | 访问手册：http://doc.likemarket.net
